ef00586456c1983616500896b9ed1c475a2e1c17
AndreiVitanescu99 <andreivitanescu99@gmail.com>
2023-11-06 13:41:57 +0200

    modify server.js for deploy

--- Backend/server.js
+++ Backend/server.js
@@ -1,22 +1,22 @@
 require('dotenv').config();
 
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
-const multer = require('multer');
-const path = require('path'); // Import the 'path' module
+// const multer = require('multer');
+// const path = require('path'); // Import the 'path' module
 
-const dbOperations = require('./dbFiles/dbUser/dbOperations');
-const userRoutes = require('./routes/userRoutes');
+// const dbOperations = require('./dbFiles/dbUser/dbOperations');
+// const userRoutes = require('./routes/userRoutes');
 
-const API_PORT = process.env.PORT || 5000;
+const API_PORT = process.env.PORT || 8080;
 
 // dbOperations.connectToDatabase();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use('/dbFiles', express.static(__dirname + '/dbFiles'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(cors());
+// app.use('/dbFiles', express.static(__dirname + '/dbFiles'));
 
 // const storage = multer.diskStorage({
 //     destination: function (req, file, cb) {
@@ -57,13 +57,13 @@ app.use('/dbFiles', express.static(__dirname + '/dbFiles'));
 //     }
 // })
 
-app.use('/users', userRoutes); // Use user-related routes
+// app.use('/users', userRoutes); // Use user-related routes
 
 // Error handling middleware
-app.use((err, req, res, next) => {
-    console.error('Error:', err);
-    res.status(500).json({ success: false, message: 'Internal server error' });
-});
+// app.use((err, req, res, next) => {
+//     console.error('Error:', err);
+//     res.status(500).json({ success: false, message: 'Internal server error' });
+// });
 
 
 app.listen(API_PORT, () => console.log(`listening on port ${API_PORT}`));
